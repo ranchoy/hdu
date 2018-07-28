3b698f3b0243932f3866e4e48ba4b82a2346a97c
ranchoy <1776004782@qq.com>
2018-07-28 17:18:14 +0800

    7-28

--- dp/hdu-1003-Max Sum-dp问题子序列和最大.cpp
+++ dp/hdu-1003-Max Sum-dp问题子序列和最大.cpp
@@ -53,3 +53,56 @@ int main()
 	}
 	return 0;
 } 
+
+
+/*
+//最大子序列和
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#define MAX 100005
+#define INF 0x7fffffff
+using namespace std;
+
+int main()
+{
+	int arr[MAX];
+	int cnt,n,res,start,end,sum,temp,cse=1;
+	scanf("%d", &cnt);
+	while(cnt--)
+	{
+		scanf("%d", &n);
+		for(int i=1; i<=n; i++)
+		{
+			scanf("%d", &arr[i]);
+		}
+
+		sum = 0;
+		res = -INF;
+		temp = start = end = 1;
+		for(int i=1; i<=n; i++)
+		{
+			if(sum<0)
+			{
+				sum = 0;
+				temp = i;
+			}
+			sum += arr[i];
+			if(sum>res) 
+			{
+				res = sum;
+				start = temp;
+				end = i;
+			}
+		}
+		printf("Case %d:\n", cse++);
+		printf("%d %d %d\n", res, start, end);
+		if(cnt>0)
+		{
+			printf("\n");
+		}
+	}
+	return 0;
+} 
+ */
