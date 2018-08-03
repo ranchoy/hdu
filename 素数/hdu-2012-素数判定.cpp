679d774e5d87cc3d6cd9bc085f07504c79396f55
ranchoy <1776004782@qq.com>
2018-08-03 14:41:05 +0800

    8-3

---
+++ 素数/hdu-2012-素数判定.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#define MAX 3000
+using namespace std;
+
+int is_prime[MAX];
+
+void init()
+{
+	is_prime[2] = 1;
+	for(int i=3; i<MAX; i++)
+	{
+		if(i%2==1)
+		{
+			is_prime[i] = 1;
+		}
+		else
+		{
+			is_prime[i] = 0;
+		}
+	}
+	
+	for(int i=3; i<MAX; i++)
+	{
+		if(is_prime[i]==1)
+		{
+			for(int j=i+i; j<MAX; j=j+i)
+			{
+				is_prime[j] = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	init();
+	int x,y,flag;
+	while(~scanf("%d %d", &x, &y) && (x+y))
+	{
+		flag = 0;
+		for(int i=x; i<=y; i++)
+		{
+			if(is_prime[i*i+i+41]==0)
+			{
+				flag = 1;
+				break;
+			} 
+		}
+		
+		if(flag==0)
+		{
+			printf("OK\n");
+		}
+		else
+		{
+			printf("Sorry\n");
+		}
+	}
+	return 0;
+} 
