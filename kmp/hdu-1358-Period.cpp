1f7b10d9cd938c8b919b238eab7e062d77dd8492
ranchoy <ranchoy@qq.com>
2018-11-23 11:36:21 +0800

    11-23

--- kmp/hdu-1358-Period.cpp
+++ kmp/hdu-1358-Period.cpp
@@ -1,11 +1,10 @@
-//题意：求最小循环节问题，可能这题数据比较特殊,大部分都是循环节由循环节构成的
-//注意循环节求法， cycle = (i+1) - ( next[i]+1 );
-//主意循环节特殊情况：len%cycle=0，可能aabaab这样，也可能是abcde这样
+// 题意：求最小循环节问题，可能这题数据比较特殊,大部分都是循环节由循环节构成的
+// 注意循环节求法， cycle = (i+1) - ( next[i]+1 );
+// 注意循环节特殊情况：len%cycle=0，可能aabaab这样，也可能是abcde这样
 #include<cstdio>
 #include<iostream>
 #include<cstring>
 #define MAX 1000005
-
 using namespace std;
 
 char str[MAX];
@@ -34,9 +33,9 @@ int main()
 {
     int len,cycle;
     int k = 0;
-    while( ~scanf("%d",&len) )
+    while(~scanf("%d",&len))
     {
-        if( len==0 )
+        if(len==0)
         {
             break;
         }
@@ -47,8 +46,8 @@ int main()
 
         for( int i=0; i<len; i++)
         {
-            cycle = (i+1) - (nexti[i]+1);//循环节
-            if((i+1)%cycle==0 && (i+1)!=cycle)//循环节要是其子串
+            cycle = (i+1) - (nexti[i]+1);// 循环节
+            if((i+1)%cycle==0 && (i+1)!=cycle)// 循环节要是其子串
             {
                 printf("%d %d\n", i+1, (i+1)/cycle);
             }
