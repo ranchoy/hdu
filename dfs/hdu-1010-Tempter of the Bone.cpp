016e1513011c7c5df82cd4b53d709fcf25b6a8a7
ranchoy <ranchoy@qq.com>
2019-04-10 11:07:58 +0800

    4-10

--- dfs/hdu-1010-Tempter of the Bone.cpp
+++ dfs/hdu-1010-Tempter of the Bone.cpp
@@ -1,41 +1,40 @@
-//dfs算法,用到剪枝,奇偶剪枝
-#include <cstdio>
-#include <cmath>
-
+// 经典的DFS走迷宫问题，奇偶剪枝
+#include<iostream>
+#include<cstring>
+#define MAX 55
 using namespace std;
 
+int vis[MAX][MAX];
+char map[MAX][MAX];
 int n,m,t,sx,sy,ex,ey,flag;
-int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
-char str[10][10];
+int dir[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-void dfs( int x, int y, int step)
+void dfs(int x, int y, int step)
 {
-
-    if( step==t && x==ex && y==ey)
+    int temp,nx,ny; 
+    
+    if(step == t && ex == x && ey == y)
     {
         flag = 1;
         return;
     }
 
     if(flag) return;
+    
+    // 奇偶剪枝
+    temp = abs(t-step) - abs(ex-x) - abs(ey-y);
+    if(temp&1 || temp<0) return;
 
-    //奇偶剪枝
-    int temp = (t-step) - abs(x-ex) - abs(y-ey);
-
-    if( temp <0 || temp&1 ){
-        return;
-    }
-
-    for( int i=0; i<4; i++)
+    for(int i=0; i<4; i++)
     {
-        int fx = x + dir[i][0];
-        int fy = y + dir[i][1];
-        if( fx>=0 && fx<n && fy>=0 && fy<m && str[fx][fy]!='X' )
+        nx = x + dir[i][0];
+        ny = y + dir[i][1];
+
+        if(nx>=0 && nx<n && ny>=0 && ny<m && map[nx][ny]!='X' && vis[nx][ny]==0)
         {
-            str[fx][fy] = 'X';
-            dfs( fx, fy, step+1);
-            str[fx][fy] = '.';//回溯
-            if(flag) return;
+            vis[nx][ny] = 1;
+            dfs(nx, ny, step+1);
+            vis[nx][ny] = 0;
         }
     }
 }
@@ -43,43 +42,63 @@ void dfs( int x, int y, int step)
 int main()
 {
     int wall;
-    while( scanf("%d %d %d",&n,&m,&t)==3&&(n||m||t) )
+    while(~scanf("%d %d %d", &n, &m, &t) && (n+m+t))
     {
-        getchar();
+        // 初始化
         wall = 0;
         flag = 0;
+        memset(vis, 0, sizeof(vis));
+
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                scanf("%c",&str[i][j]);
-
-                if( str[i][j]=='S' ){
+                scanf(" %c", &map[i][j]);
+                if(map[i][j] == 'S')
+                {
                     sx = i;
                     sy = j;
-                }else if( str[i][j]=='D' ){
+                }
+                else if(map[i][j] == 'D')
+                {
                     ex = i;
                     ey = j;
-                }else if( str[i][j]=='X' ){
-                    wall ++;
                 }
+                else if(map[i][j] == 'X')
+                {
+                    wall++;
+                }    
             }
-            getchar();
         }
-
-        //剪枝
-        if( n*m - wall <= t){
+        
+        if(n*m - wall <= t)
+        {
             printf("NO\n");
-        }else{
-            str[sx][sy] = 'X';
-            dfs( sx, sy, 0);
-
-            if(flag){
-                printf("YES\n");
-            }else{
-                printf("NO\n");
-            }
+        }
+        else
+        {
+            vis[sx][sy] = 1;
+            dfs(sx, sy, 0);
+            
+            if(flag) printf("YES\n");
+            else printf("NO\n");
         }
     }
     return 0;
 }
+
+/*
+4 4 5
+S.X.
+..X.
+..XD
+....
+3 4 5
+S.X.
+..X.
+...D
+0 0 0
+
+NO
+YES
+*/
